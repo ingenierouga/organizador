ea0000a4c61c83705b6f53b03469c89262764b90
Emilio Sanchez <ingenierouga@gmail.com>
2014-08-01 12:07:27 -0500

    Commit viernes 1 agosto

--- app/controllers/empresas_controller.rb
+++ app/controllers/empresas_controller.rb
@@ -4,9 +4,20 @@ class EmpresasController < ApplicationController
   end
 
   def show
+    @empresa = Empresa.find(params[:id])
   end
 
   def new
+    @empresa = Empresa.new
+    @empresa.nombre = params[:nombre]
+    @empresa.calle = params[:calle]
+    @empresa.colonia = params[:colonia]
+    @empresa.numero_ext = params[:numero_ext]
+    @empresa.numero_int = params[:numero_int]
+    @empresa.codigo_postal = params[:codigo_postal]
+    @empresa.ciudad = params[:ciudad]
+    @empresa.pais = params[:pais]
+    @empresa.save
   end
 
   def create
@@ -19,5 +30,8 @@ class EmpresasController < ApplicationController
   end
 
   def destroy
+     @empresa = Empresa.find(params[:id])
+     @empresa.destroy!
+     redirect_to index
   end
 end
